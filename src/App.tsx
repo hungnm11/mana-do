3519db341131c5407dd1c82b9d743118db9113cc
Hung Nguyen <hung.nguyen@hotmail.com.vn>
2021-05-13 10:41:53 +0700

    store todo list

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import SignInPage from './components/signin/SignInPage';
-import ToDoPage from './ToDoPage';
+import ToDoPage from './components/todo/ToDoPage';
 
 import './App.css';
 
